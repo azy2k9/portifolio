6e14722261e805a2657e6cfdb67f9612398a9bbc
Arslaan Qadus <azy2k9@gmail.com>
2020-05-16 10:58:00 +0100

    Added the logo

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -7,13 +7,21 @@ const Container = styled.div`
   height: 64px;
   display: flex;
   justify-content: space-between;
-  border-bottom: 1px solid ${({ theme }) => (theme.colors.secondary)};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.secondary};
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 33px;
 `;
 
 const Header: React.FC = () => {
   return (
     <Container>
-      <Logo />
+      <LogoContainer>
+        <Logo />
+      </LogoContainer>
       <Navbar />
     </Container>
   );

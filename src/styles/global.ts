71b048713db5a77e70525d3de27a69795ba182fb
Arslaan Qadus <azy2k9@gmail.com>
2020-05-16 18:43:14 +0100

    Resets and overflow

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,13 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-body {
-    background: ${({ theme }) => theme.colors.background};
+*,
+*::after,
+*::before {
+    box-sizing: border-box;
+}
+
+html, body, #root {
     height: 100vh;
     margin: 0;
     padding: 0;
     font-family: ${({ theme }) => theme.font.fontFamily.primary};
-    transition: all 0.25s linear;
     overflow: hidden;
+    display: flex;
+    flex-direction: column;
 }
 `;

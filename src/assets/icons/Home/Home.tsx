a1acf65d8b54c05b11c14dac649b983ca33528a0
Gil Alvaro <gil.pita@gmail.com>
2020-05-16 19:11:34 +0100

    Added Icons

--- src/assets/icons/Home/Home.tsx
+++ src/assets/icons/Home/Home.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <svg width="24" height="21" viewBox="0 0 24 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M10 17V11H14V17H19V9H22L12 0L2 9H5V17H10Z" fill="currentColor" />
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10 20V14H14V20H19V12H22L12 3L2 12H5V20H10Z" fill="currentColor" />
     </svg>
+
   );
 };
 

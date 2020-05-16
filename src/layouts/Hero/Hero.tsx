24343a5200644c461d1c5666f0f44a7a50f39de1
Arslaan Qadus <azy2k9@gmail.com>
2020-05-16 19:49:20 +0100

    Added and started utilising Typography component

--- src/layouts/Hero/Hero.tsx
+++ src/layouts/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import HeroIcon from '../../assets/icons/Hero';
 import LogoAlternate from '../../assets/icons/LogoAlternate';
+import Typography from '../../components/Typography';
 
 const Container = styled.div`
   display: flex;
@@ -25,10 +26,20 @@ const HeroInfoContainer = styled.div`
   width: 100%;
   align-items: flex-start;
   padding-left: 5rem;
+  flex-direction: column;
 
   @media (max-width: 768px) {
     padding-left: 3.5rem;
   }
+`;
+
+const TextContainer = styled.div`
+  padding-top: 2rem;
+`;
+
+const Link = styled.a`
+  color: ${({ theme }) => theme.colors.primary};
+  text-decoration: none;
 `;
 
 const Hero: React.FC = () => {
@@ -37,6 +48,23 @@ const Hero: React.FC = () => {
       <HeroIcon />
       <HeroInfoContainer>
         <LogoAlternate />
+        <TextContainer>
+          <Typography headingSize headingColor>
+            Arslaan Qadus
+          </Typography>
+          <br />
+          <Typography headingSize headingColor>
+            Front End Developer
+          </Typography>
+          <br />
+          <Typography headingColor>
+            Living in Manchester 
+          </Typography>
+          <br />
+          <Typography headingColor>
+            Working @ <Link href="https://www.connexone.co.uk">ConnexOne</Link>
+          </Typography>
+        </TextContainer>
       </HeroInfoContainer>
     </Container>
   );

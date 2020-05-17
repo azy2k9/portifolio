cbf6fa0c893ab087f16ab837a2f4a3487b2e4fa9
Arslaan Qadus <azy2k9@gmail.com>
2020-05-17 19:15:23 +0100

    formatting

--- src/components/atoms/HamburgerButton/HamburgerButton.tsx
+++ src/components/atoms/HamburgerButton/HamburgerButton.tsx
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 
 const StyledHamburgerIcon = styled.svg<Props>`
   .line {
-    fill:none;
+    fill: none;
     transition: stroke-dasharray 400ms, stroke-dashoffset 400ms, transform 400ms;
     stroke: ${({ theme }) => theme.colors.paragraph};
-    stroke-width:5.5;
-    stroke-linecap:round;
+    stroke-width: 5.5;
+    stroke-linecap: round;
     transform: ${({ isOpen }) => (isOpen ? 'rotate(45deg)' : 'rotate(0)')};
     transform-origin: 50% 50%;
   }
@@ -23,13 +23,11 @@ const StyledHamburgerIcon = styled.svg<Props>`
   }
 `;
 
-
 interface Props {
-  isOpen: boolean,
+  isOpen: boolean;
 }
 
 const HamburgerButton: React.FC<Props> = (props: Props) => {
-
   return (
     <StyledHamburgerIcon
       viewBox="0 0 100 100"
@@ -37,9 +35,15 @@ const HamburgerButton: React.FC<Props> = (props: Props) => {
       width="42"
       {...props}
     >
-      <path className="line top" d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
+      <path
+        className="line top"
+        d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40"
+      />
       <path className="line middle" d="m 30,50 h 40" />
-      <path className="line bottom" d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
+      <path
+        className="line bottom"
+        d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40"
+      />
     </StyledHamburgerIcon>
   );
 };

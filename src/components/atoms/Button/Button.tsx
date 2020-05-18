50340265cdff5fb816ee9e77e0fb9a65d08bdd89
Gil Alvaro <gil.pita@gmail.com>
2020-05-18 21:16:49 +0100

    Styled Button

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { rgba, lighten } from 'polished';
 
 const ButtonContainer = styled.button<Button>`
   background-color: ${({ primary, theme }) => primary && theme.colors.primary};
   color: ${({ primary, theme }) => primary && theme.colors.white};
+  border: 0;
+  box-shadow: 0 2px 8px -1px ${({ theme }) => rgba(theme.colors.primary, 0.4)};
   border-radius: ${({ rounded, children }) =>
-    rounded && children && `${children?.toString().length * 3}px`};
+    rounded && children && '9999px'};
   cursor: pointer;
   letter-spacing: 0.1rem;
   padding: 8px 30px;
+  outline: none;
   font-size: ${({ theme }) => `${theme.font.fontSize.small}px`};
   font-weight: ${({ theme }) => `${theme.font.fontWeight.ultralight}`};
-
+  transition: box-shadow 0.3s ease, background-color 0.3s ease, transform 0.3s ease;
   :hover {
-    border: ${({ theme }) => `2px solid ${theme.colors.secondary}`};
+    border: 0;
+    background-color: ${({ primary, theme }) => primary && lighten(0.05, theme.colors.primary)};
+    box-shadow: 0 4px 20px -2px ${({ theme }) => rgba(theme.colors.primary, 0.5)};
+    transform: scale(1.05);
   }
 `;
 

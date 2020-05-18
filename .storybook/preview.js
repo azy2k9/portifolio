4a8484fccca685e6c5a571259693016eb516902a
Arslaan Qadus <azy2k9@gmail.com>
2020-05-18 19:43:43 +0100

    Add viewport storybook addon, remove old config.js and replace with preview.js, remove manager.js and import the addon properly in main.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
+import { addParameters, addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme } from '../src/styles/theme';
 import { GlobalStyles } from '../src/styles/global';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  align-self: center;
-  width: 100vw;
-  height: 100vh;
-`;
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const GlobalWrapper = (storyFn) => (
   <ThemeProvider theme={lightTheme}>
     <GlobalStyles />
-    <Container>{storyFn()}</Container>
+    {storyFn()}
   </ThemeProvider>
 );
+
+addParameters({
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+    },
+  },
+});
 
 addDecorator(GlobalWrapper);

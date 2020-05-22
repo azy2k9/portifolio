65595a68cf518ba6976ca0f21e557f39026e59ec
Gil Alvaro <gil.pita@gmail.com>
2020-05-22 20:39:50 +0100

    Created Files

---
+++ src/components/molecules/Portrait/Portrait.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+
+} from './Portrait.styled';
+
+const Portrait: React.FC<Portrait> = ({
+
+}: Card) => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+export default Portrait;

cc11fe5b1b93b3cf273c278411bd67b3efc56ae9
Arslaan Qadus <azy2k9@gmail.com>
2020-05-25 12:25:56 +0100

    Create a resuable Chart component

---
+++ src/components/molecules/Chart/Chart.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis } from 'recharts';
+
+interface Props {
+  data: Array<Object>;
+  color: string;
+}
+
+const Chart: React.FC<Props> = ({ data, color }: Props) => {
+  return (
+    <RadarChart
+      cx={300}
+      cy={250}
+      outerRadius={150}
+      width={600}
+      height={500}
+      data={data}
+    >
+      <PolarGrid />
+      <PolarAngleAxis dataKey="subject" />
+      <Radar
+        dataKey="skill"
+        stroke="#fff"
+        fill={color}
+        color="#fff"
+        fillOpacity={0.9}
+      />
+    </RadarChart>
+  );
+};
+
+export default Chart;
